6fa7eb081e869989a0007b24cc494bc6541cf704
lpast <lmarco@outlook.es>
2023-01-10 00:41:55 +0530

    first commit

--- php/class/interfaz.php
+++ php/class/interfaz.php
@@ -191,6 +191,14 @@ class Interfaz {
   }
 }
 
+  static public function imagen_aleatoria(){
+    echo "<div class='container-fluid'>
+    <div class='row'>
+      <div class='col-xs-12 col-sm-8 col-sm-offset-2 menu-inicio'>
+        <h1 align='center'> Encuentra tu nuevo hogar</h1>";
+          $conexion = abrirConexion();
+          $sql = 'SELECT imagen FROM tbl_inmuebles';
+          $imagenes = array();
 
 static public function mostrarMenu() {if (isset($_SESSION['tipo'])) {
   $tipo_usuario = $_SESSION['tipo'];
