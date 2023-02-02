dec21b184f433e0f0490c7ed97b5495ecd7ac87e
lpast <root@ubuntu>
2023-02-02 23:55:30 +0530

    ...

--- cerrar_sesion.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	session_start();
-	setcookie('datos','',time()-1,'/'); // Elimino la cookie
-	$_SESSION = array(); // Limpio el array $_SESSION
-	session_destroy(); // Elimino la sesión
-	header('Location: ../index.html');
- ?>
